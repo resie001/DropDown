44c02ae5e03fa3544c1921a3ece684072a1bee06
ade.resie <ade.resie@privyid.tech>
2022-11-22 17:10:36 +0700

    fix appearance

--- DropDown/src/DropDown+Appearance.swift
+++ DropDown/src/DropDown+Appearance.swift
@@ -28,7 +28,7 @@ extension DropDown {
 		appearance.textColor = DPDConstant.UI.TextColor
         appearance.selectedTextColor = DPDConstant.UI.SelectedTextColor
 		appearance.textFont = DPDConstant.UI.TextFont
-        appearance.height = DPDConstant.UI.Height
+        appearance.dropdownHeight = DPDConstant.UI.Height
 	}
 
 }
